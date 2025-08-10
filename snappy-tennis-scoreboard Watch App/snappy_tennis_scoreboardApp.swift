45069b8d97148b34ae4412909e2afab12d59584c
Trevor Wolf <t@trev.io>
2025-08-10 19:37:54 +1000

    Initial Commit

---
+++ snappy-tennis-scoreboard Watch App/snappy_tennis_scoreboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  snappy_tennis_scoreboardApp.swift
+//  snappy-tennis-scoreboard Watch App
+//
+//  Created by aldus on 10/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct snappy_tennis_scoreboard_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ snappy-tennis-scoreboard Watch AppTests/snappy_tennis_scoreboard_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  snappy_tennis_scoreboard_Watch_AppTests.swift
+//  snappy-tennis-scoreboard Watch AppTests
+//
+//  Created by aldus on 10/8/2025.
+//
+
+import Testing
+@testable import snappy_tennis_scoreboard_Watch_App
+
+struct snappy_tennis_scoreboard_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

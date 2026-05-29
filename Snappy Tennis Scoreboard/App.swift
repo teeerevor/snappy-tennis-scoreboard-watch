aa83efc8448305fd048e5b021a2003621949742b
Trevor Wolf <t@trev.io>
2026-05-29 23:32:29 +1000

    able to build and deploy again

---
+++ Snappy Tennis Scoreboard/App.swift
@@ -0,0 +1,18 @@
+//
+//  app.swift
+//  snappy-tennis-scoreboard
+//
+//  Created by boris on 28/5/2026.
+//
+
+
+import SwiftUI
+
+@main
+struct SnappyTennisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Get the app on Apple Watch")
+        }
+    }
+}
